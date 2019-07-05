24d88af3ae22d8eb27f9073bade2424a68cbc7a0
0ddlyoko <0ddlyokoOfficial@gmail.com>
2019-07-05 17:19:35 +0200

    Server implementation

--- src/main/java/com/froxynetwork/servermanager/Main.java
+++ src/main/java/com/froxynetwork/servermanager/Main.java
@@ -1,9 +1,15 @@
 package com.froxynetwork.servermanager;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.froxynetwork.froxynetwork.network.NetworkManager;
+import com.froxynetwork.servermanager.server.ServerManager;
 
 import lombok.Getter;
 
@@ -36,22 +42,51 @@ public class Main {
 
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
 
+	private Properties p;
+
 	@Getter
 	private NetworkManager networkManager;
+	@Getter
+	private ServerManager serverManager;
 
 	public Main(String[] args) {
 		LOG.info("ServerManager initialization");
-		if (args == null || args.length != 3) {
-			LOG.error("Invalid argument number, please enter correct arguments ! (<url> <client_id> <client_secret>)");
+		if (args == null || args.length != 1) {
+			LOG.error("Invalid argument number, please enter correct arguments ! (<propertiesFile>)");
 			System.exit(1);
 		}
-		String url = args[0];
-		String clientId = args[1];
-		String clientSecret = args[2];
-		if (LOG.isInfoEnabled()) {
-			LOG.info("url = {}, client_id = {}, client_secret = {}", url, clientId, clientSecret == null ? "null" : "<hidden>");
-			LOG.info("Initializing NetworkManager");
+		String properties = args[0];
+		File fProperties = new File(properties);
+		if (fProperties == null || !fProperties.exists()) {
+			LOG.error("Properties file not found ({})", properties);
+			System.exit(1);
 		}
+		if (!fProperties.isFile() || !fProperties.canRead()) {
+			LOG.error("Properties file is not a file or we don't have permission to read the properties file ({})",
+					properties);
+			System.exit(1);
+		}
+		p = new Properties();
+		try {
+			p.load(new FileInputStream(fProperties));
+		} catch (IOException ex) {
+			LOG.error("Error while reading properties file ({})", properties);
+			LOG.error("", ex);
+			System.exit(1);
+		}
+
+		initializeNetwork();
+		initializeServer();
+		LOG.info("All initialized");
+	}
+
+	private void initializeNetwork() {
+		LOG.info("Initializing NetworkManager");
+		String url = p.getProperty("url");
+		String clientId = p.getProperty("client_id");
+		String clientSecret = p.getProperty("client_secret");
+		LOG.info("url = {}, client_id = {}, client_secret = {}", url, clientId,
+				clientSecret == null ? "null" : "<hidden>");
 		try {
 			networkManager = new NetworkManager(url, clientId, clientSecret);
 		} catch (Exception ex) {
@@ -59,10 +94,74 @@ public class Main {
 			System.exit(1);
 		}
 		LOG.info("NetworkManager initialized");
+	}
+
+	private void initializeServer() {
+		LOG.info("Initializing ServerManager");
+		String from = p.getProperty("srvDir");
+		String to = p.getProperty("toDir");
+		String lPort = p.getProperty("lowPort");
+		String hPort = p.getProperty("highPort");
+		String dDirectories = p.getProperty("deleteDirectories");
+		String dFiles = p.getProperty("deleteFiles");
+		if (from == null || "".equalsIgnoreCase(from.trim())) {
+			LOG.error("Incorrect config ! (srvDir is empty)");
+			System.exit(1);
+		}
+		if (to == null || "".equalsIgnoreCase(to.trim())) {
+			LOG.error("Incorrect config ! (toDir is empty)");
+			System.exit(1);
+		}
+		if (lPort == null || "".equalsIgnoreCase(lPort.trim())) {
+			LOG.error("Incorrect config ! (lowPort is empty)");
+			System.exit(1);
+		}
+		if (hPort == null || "".equalsIgnoreCase(hPort.trim())) {
+			LOG.error("Incorrect config ! (highPort is empty)");
+			System.exit(1);
+		}
+		LOG.info("srvDir = {}, toDir = {}, lowPort = {}, highPort = {}, deleteDirectories = {}, deleteFiles = {}", from,
+				to, lPort, hPort, dDirectories, dFiles);
+		File srvDir = new File(from);
+		File toDir = new File(to);
+		if (from == null || !srvDir.isDirectory()) {
+			LOG.error("srvDir is not a directory ({})", from);
+			System.exit(1);
+		}
+		if (to == null || !toDir.isDirectory()) {
+			LOG.error("toDir is not a directory ({})", from);
+			System.exit(1);
+		}
+		int lowPort = 25566;
+		try {
+			lowPort = Integer.parseInt(lPort);
+		} catch (NumberFormatException ex) {
+			LOG.error("lowPort is not a number: {}", lPort);
+			LOG.info("Using default lowPort ({})", lowPort);
+		}
+		int highPort = lowPort + 100;
+		try {
+			highPort = Integer.parseInt(hPort);
+		} catch (NumberFormatException ex) {
+			LOG.error("highPort is not a number: {}", hPort);
+			LOG.info("Using default highPort ({})", highPort);
+		}
+		boolean deleteDirectories = false;
+		if ("true".equalsIgnoreCase(dDirectories))
+			deleteDirectories = true;
+		boolean deleteFiles = false;
+		if ("true".equalsIgnoreCase(dFiles))
+			deleteFiles = true;
+		serverManager = new ServerManager(this, srvDir, toDir, lowPort, highPort, deleteDirectories, deleteFiles);
 		LOG.info("ServerManager initialized");
 	}
 
 	public static void main(String[] args) {
-		new Main(args);
+		Main main = new Main(args);
+		main.getServerManager().openServer("test", srv -> {
+			System.out.println("Done: " + srv);
+		}, () -> {
+			System.out.println("ERROR");
+		});
 	}
 }
---
+++ src/main/java/com/froxynetwork/servermanager/server/ServerManager.java
@@ -0,0 +1,195 @@
+package com.froxynetwork.servermanager.server;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.function.Consumer;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.froxynetwork.froxynetwork.network.output.Callback;
+import com.froxynetwork.froxynetwork.network.output.RestException;
+import com.froxynetwork.froxynetwork.network.output.data.server.ServerDataOutput;
+import com.froxynetwork.froxynetwork.network.output.data.server.ServerDataOutput.Server;
+import com.froxynetwork.servermanager.Main;
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 FroxyNetwork
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * @author 0ddlyoko
+ */
+public class ServerManager {
+
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
+	private Main main;
+
+	// Servers with id
+	private HashMap<Integer, Server> servers;
+	// Directory where are servers to copy
+	private File srvDir;
+	// Directory where will be servers
+	private File toDir;
+	// The lowest port to use
+	private int lowPort;
+	// The highest port to use
+	private int highPort;
+	// Used to check the availability of ports
+	private boolean[] availablePort;
+
+	public ServerManager(Main main, File srvDir, File toDir, int lowPort, int highPort, boolean deleteDirectories,
+			boolean deleteFiles) {
+		servers = new HashMap<>();
+		this.main = main;
+		this.srvDir = srvDir;
+		this.toDir = toDir;
+		this.lowPort = lowPort;
+		this.highPort = highPort;
+		this.availablePort = new boolean[highPort - lowPort + 1];
+		for (int i = 0; i < availablePort.length; i++)
+			availablePort[i] = true;
+		// Delete all servers
+		boolean error = false;
+		LOG.info("deleteDirectories = {}, deleteFiles = {}", deleteDirectories, deleteFiles);
+		LOG.info("Number of directories / files = {}", toDir.list().length);
+		int nbrDirectories = 0;
+		int nbrFiles = 0;
+		for (File f : toDir.listFiles()) {
+			try {
+				if (f.isDirectory()) {
+					nbrDirectories++;
+					if (deleteDirectories) {
+						LOG.info("Deleting directory {}", f.getName());
+						FileUtils.deleteDirectory(f);
+					} else
+						LOG.warn("Skipping directory {}", f.getName());
+				} else if (f.isFile()) {
+					nbrFiles++;
+					if (deleteFiles) {
+						LOG.info("Deleting file {}", f.getName());
+						f.delete();
+					} else
+						LOG.warn("Skipping file {}", f.getName());
+				}
+			} catch (Exception ex) {
+				// Error
+				error = true;
+				LOG.error("An error has occured while deleting the server directory {}", f.getName());
+				LOG.error("", ex);
+			}
+		}
+		if (deleteDirectories)
+			LOG.info("{} directories deleted !", nbrDirectories);
+		else
+			LOG.info("{} directories passed !", nbrDirectories);
+		if (deleteFiles)
+			LOG.info("{} files deleted !", nbrFiles);
+		else
+			LOG.info("{} files passed !", nbrFiles);
+		if (error)
+			throw new IllegalStateException("An error has occured while trying to load ServerManager");
+	}
+
+	/**
+	 * Try to open a server
+	 * 
+	 * @param type
+	 *            The type of server (HUB, KOTH, ...)
+	 * @param then
+	 *            The action to execute once the server is created and launched
+	 * @param error
+	 *            The action to execute if error occures
+	 */
+	public synchronized void openServer(String type, Consumer<Server> then, Runnable error) {
+		LOG.info("Opening a new server (type = {})", type);
+		// Get port
+		int port = -1;
+		for (int i = 0; i < availablePort.length && port == -1; i++)
+			if (availablePort[i])
+				port = i + lowPort;
+		if (port == -1) {
+			// Not available port
+			LOG.error("Not available port (opened)");
+			error.run();
+			return;
+		}
+		openServer(type, port, then, error);
+	}
+
+	private void openServer(String type, int port, Consumer<Server> then, Runnable error) {
+		availablePort[port - lowPort] = false;
+		LOG.info("Using port {}", port);
+		main.getNetworkManager().network().getServerService().asyncAddServer(type.toUpperCase() + "_" + port, port,
+				new Callback<ServerDataOutput.Server>() {
+
+					@Override
+					public void onResponse(Server response) {
+						// Ok
+						LOG.info("Server created on REST server, id = {}, creationTime = {}", response.getId(),
+								response.getCreationTime());
+						// TODO
+						then.accept(response);
+					}
+
+					@Override
+					public void onFailure(RestException ex) {
+						// Error
+						LOG.error(
+								"Error while asking REST server for type = {} and port = {}: return code = {}, error message = {}",
+								type, port, ex.getError().getCode(), ex.getError().getErrorMessage());
+						LOG.error("", ex);
+						availablePort[port - lowPort] = true;
+						LOG.info("Port {} is now free", port);
+						error.run();
+					}
+
+					@Override
+					public void onFatalFailure(Throwable t) {
+						// Fatal
+						LOG.error("Fatal error while asking REST server for type = {} and port = {}", type, port);
+						availablePort[port - lowPort] = true;
+						LOG.error("Port {} is now free", port);
+					}
+				});
+	}
+
+	/**
+	 * Close a server
+	 * 
+	 * @param srv
+	 *            The server
+	 */
+	public void closeServer(Server srv) {
+
+	}
+
+	/**
+	 * Close a server
+	 * 
+	 * @param id
+	 *            The id of the server
+	 */
+	public void closeServer(int id) {
+		closeServer(servers.get(id));
+	}
+}
